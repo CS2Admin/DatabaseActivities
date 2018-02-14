fbca919d275c1d411de973bbeb0cc2ccdd772445
CS2Admin <CS2Teacher@gmail.com>
2018-02-14 09:18:56 -0600

    finished examples

--- DatabaseActivities/Controllers/FirstController.cs
+++ DatabaseActivities/Controllers/FirstController.cs
@@ -11,7 +11,24 @@ namespace DatabaseActivities.Controllers
         // GET: First
         public ActionResult Index()
         {
-            return View();
+            return View(viewName:"Index",model:"Hello");
+        }
+
+        [HttpPost]
+        public ActionResult SayHello(string inputString)
+        {
+            return View(viewName: "Index", model: "Hello " + inputString);
+        }
+
+        public ActionResult Square()
+        {
+            return View("Square", 0.0);
+        }
+
+        [HttpPost]
+        public ActionResult SquareNumber(double inputNumber)
+        {
+            return View("Square", inputNumber * inputNumber);
         }
     }
 }
